58661efc0563abedd9f2edfa75afee2458d962cf
bashar <mdsaimunallbashar@gmail.com>
2023-11-01 15:06:37 +0600

    added new section

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+   loop:true,
+   margin:30,
+   nav:true,
+   dots:false,
+   autoplay: true,
+   autoplayTimeout:3000,
+   responsive:{
+       0:{
+           items:1
+       }
+      
+   }
+})
